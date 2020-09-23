3a443bfe84ded25d0e2fe72c6264ec0c1620d9cd
snorbi2 <snorbi2@gmail.com>
2020-09-23 17:14:56 +0200

    commit 3

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +22,21 @@ namespace UserMaintenance
             label1.Text = Resource.LastName;
             label2.Text = Resource.FirstName;
             button1.Text = Resource.Add;
+
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = label1.Text,
+                FirstName = label2.Text
+            };
+            users.Add(u);
         }
     }
 }
