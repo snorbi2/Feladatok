d6bea687e1d765c4a21f2383fd4c47be0884e61b
snorbi2 <snorbi2@gmail.com>
2020-10-11 12:43:23 +0200

    elso commit

---
+++ UserMaintenance/gyak5/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gyak5
+{
+    public partial class Form1 : Form
+    {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+
+
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
+        }
+
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim()
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
+        }
+    }
+}
