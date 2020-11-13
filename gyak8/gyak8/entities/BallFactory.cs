fca63ebac6c98664cf9f2f4492b75ab7421d8f7c
snorbi2 <snorbi2@gmail.com>
2020-11-13 13:26:15 +0100

    masodik commit

---
+++ gyak8/gyak8/entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak8.entities
+{
+    public class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
